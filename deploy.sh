29a7a80ead69fe4a43466fa74605b097535c0b2a
YoursTruly450 <yourstruly.bill@gmail.com>
2022-01-18 13:16:40 +0400

    fix: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m "New Deployment"
-git push -f github.com/YoursTruly450/weather-vue-typescript main:gh-pages
+git push -f git@github.com:YoursTruly450/weather-vue-typescript.git master:gh-pages
 
 cd -
